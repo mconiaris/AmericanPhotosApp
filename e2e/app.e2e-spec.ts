e67f9a143ee73ed7638ee130117e392923f805e9
mconiaris <mconiaris@gmail.com>
2018-04-03 11:51:39 -0400

    Modifed e2e test to display right text.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('american-photos-app App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to American Photos!');
   });
 });
